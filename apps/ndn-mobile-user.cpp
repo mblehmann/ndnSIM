2970dfbaaf86f5822f40646d54d01d7aeb56e6eb
Matheus Brenner Lehmann <mblehmann@inf.ufrgs.br>
2016-01-07 13:35:20 -0200

    Consumer part of mobile user
    The mobile user is re-organized to have only the consumer part. The
    consumer needs to be extended to consider multiple objects and chunks.

--- apps/ndn-mobile-user.cpp
+++ apps/ndn-mobile-user.cpp
@@ -26,22 +26,8 @@
 #include "ns3/packet.h"
 #include "ns3/callback.h"
 #include "ns3/string.h"
-#include "ns3/boolean.h"
 #include "ns3/uinteger.h"
-#include "ns3/integer.h"
 #include "ns3/double.h"
-
-#include "utils/ndn-ns3-packet-tag.hpp"
-#include "model/ndn-app-face.hpp"
-#include "utils/ndn-rtt-mean-deviation.hpp"
-#include "model/ndn-ns3.hpp"
-#include "model/ndn-l3-protocol.hpp"
-#include "helper/ndn-fib-helper.hpp"
-
-#include <boost/lexical_cast.hpp>
-#include <boost/ref.hpp>
-
-#include <memory>
 
 NS_LOG_COMPONENT_DEFINE("ndn.MobileUser");
 
@@ -58,62 +44,17 @@ MobileUser::GetTypeId(void)
       .SetGroupName("Ndn")
       .SetParent<App>()
       .AddConstructor<MobileUser>()
-      .AddAttribute("Prefix", "Prefix, for which producer has the data", StringValue("/"),
-                    MakeNameAccessor(&MobileUser::m_prefix), MakeNameChecker())
-      .AddAttribute(
-         "Postfix",
-         "Postfix that is added to the output data (e.g., for adding producer-uniqueness)",
-         StringValue("/"), MakeNameAccessor(&MobileUser::m_postfix), MakeNameChecker())
-      .AddAttribute("PayloadSize", "Virtual payload size for Content packets", UintegerValue(1024),
-                    MakeUintegerAccessor(&MobileUser::m_virtualPayloadSize),
-                    MakeUintegerChecker<uint32_t>())
-      .AddAttribute("Freshness", "Freshness of data packets, if 0, then unlimited freshness",
-                    TimeValue(Seconds(0)), MakeTimeAccessor(&MobileUser::m_freshness),
-                    MakeTimeChecker())
-      .AddAttribute(
-         "Signature",
-         "Fake signature, 0 valid signature (default), other values application-specific",
-         UintegerValue(0), MakeUintegerAccessor(&MobileUser::m_signature),
-         MakeUintegerChecker<uint32_t>())
-      .AddAttribute("KeyLocator",
-                    "Name to be used for key locator.  If root, then key locator is not used",
-                    NameValue(), MakeNameAccessor(&MobileUser::m_keyLocator), MakeNameChecker())
-
-      .AddAttribute("StartSeq", "Initial sequence number", IntegerValue(0),
-                    MakeIntegerAccessor(&MobileUser::m_seq), MakeIntegerChecker<int32_t>())
-
-      .AddAttribute("Prefix", "Name of the Interest", StringValue("/"),
-                    MakeNameAccessor(&MobileUser::m_interestName), MakeNameChecker())
-      .AddAttribute("LifeTime", "LifeTime for interest packet", StringValue("2s"),
-                    MakeTimeAccessor(&MobileUser::m_interestLifeTime), MakeTimeChecker())
-
-      .AddAttribute("RetxTimer",
-                    "Timeout defining how frequent retransmission timeouts should be checked",
-                    StringValue("50ms"),
-                    MakeTimeAccessor(&MobileUser::GetRetxTimer, &MobileUser::SetRetxTimer),
-                    MakeTimeChecker())
-
-      .AddTraceSource("LastRetransmittedInterestDataDelay",
-                      "Delay between last retransmitted Interest and received Data",
-                      MakeTraceSourceAccessor(&MobileUser::m_lastRetransmittedInterestDataDelay),
-                      "ns3::ndn::MobileUser::LastRetransmittedInterestDataDelayCallback")
-
-      .AddTraceSource("FirstInterestDataDelay",
-                      "Delay between first transmitted Interest and received Data",
-                      MakeTraceSourceAccessor(&MobileUser::m_firstInterestDataDelay),
-                      "ns3::ndn::MobileUser::FirstInterestDataDelayCallback");
+      .AddAttribute("InterestQueue", "Queue of next interest packets to send", StringValue(),
+                    MakeNameAccessor(&MobileUser::m_interestQueue), MakeNameChecker())
+      .AddAttribute("WindowsSize", "Interest windows size", IntegerValue(0),
+                    MakeIntegerAccessor(&MobileUser::m_windowsSize), MakeIntegerChecker<int32_t>())
 
   return tid;
 }
 
 MobileUser::MobileUser()
-  : m_rand(CreateObject<UniformRandomVariable>())
-  , m_seq(0)
-  , m_seqMax(0) // don't request anything
+  : m_windowsSize(1)
 {
-  NS_LOG_FUNCTION_NOARGS();
-
-  m_rtt = CreateObject<RttMeanDeviation>();
 }
 
 // inherited from Application base class.
@@ -138,225 +79,94 @@ MobileUser::StopApplication()
   App::StopApplication();
 }
 
-void
-MobileUser::OnInterest(shared_ptr<const Interest> interest)
-{
-  App::OnInterest(interest); // tracing inside
-
-  NS_LOG_FUNCTION(this << interest);
-
-  if (!m_active)
-    return;
-
-  Name dataName(interest->getName());
-  // dataName.append(m_postfix);
-  // dataName.appendVersion();
-
-  auto data = make_shared<Data>();
-  data->setName(dataName);
-  data->setFreshnessPeriod(::ndn::time::milliseconds(m_freshness.GetMilliSeconds()));
-
-  data->setContent(make_shared< ::ndn::Buffer>(m_virtualPayloadSize));
-
-  Signature signature;
-  SignatureInfo signatureInfo(static_cast< ::ndn::tlv::SignatureTypeValue>(255));
-
-  if (m_keyLocator.size() > 0) {
-    signatureInfo.setKeyLocator(m_keyLocator);
-  }
-
-  signature.setInfo(signatureInfo);
-  signature.setValue(::ndn::nonNegativeIntegerBlock(::ndn::tlv::SignatureValue, m_signature));
-
-  data->setSignature(signature);
-
-  NS_LOG_INFO("node(" << GetNode()->GetId() << ") responding with Data: " << data->getName());
-
-  // to create real wire encoding
-  data->wireEncode();
-
-  m_transmittedDatas(data, this, m_face);
-  m_face->onReceiveData(*data);
-}
-
-void
-MobileUser::SetRetxTimer(Time retxTimer)
-{
-  m_retxTimer = retxTimer;
-  if (m_retxEvent.IsRunning()) {
-    // m_retxEvent.Cancel (); // cancel any scheduled cleanup events
-    Simulator::Remove(m_retxEvent); // slower, but better for memory
-  }
-
-  // schedule even with new timeout
-  m_retxEvent = Simulator::Schedule(m_retxTimer, &MobileUser::CheckRetxTimeout, this);
-}
-
-Time
-MobileUser::GetRetxTimer() const
-{
-  return m_retxTimer;
-}
-
-void
-MobileUser::CheckRetxTimeout()
-{
-  Time now = Simulator::Now();
-
-  Time rto = m_rtt->RetransmitTimeout();
-  // NS_LOG_DEBUG ("Current RTO: " << rto.ToDouble (Time::S) << "s");
-
-  while (!m_seqTimeouts.empty()) {
-    SeqTimeoutsContainer::index<i_timestamp>::type::iterator entry =
-      m_seqTimeouts.get<i_timestamp>().begin();
-    if (entry->time + rto <= now) // timeout expired?
-    {
-      uint32_t seqNo = entry->seq;
-      m_seqTimeouts.get<i_timestamp>().erase(entry);
-      OnTimeout(seqNo);
-    }
-    else
-      break; // nothing else to do. All later packets need not be retransmitted
-  }
-
-  m_retxEvent = Simulator::Schedule(m_retxTimer, &MobileUser::CheckRetxTimeout, this);
-}
-
-void
-MobileUser::SendPacket()
-{
-  if (!m_active)
-    return;
-
-  NS_LOG_FUNCTION_NOARGS();
-
-  uint32_t seq = std::numeric_limits<uint32_t>::max(); // invalid
-
-  while (m_retxSeqs.size()) {
-    seq = *m_retxSeqs.begin();
-    m_retxSeqs.erase(m_retxSeqs.begin());
-    break;
-  }
-
-  if (seq == std::numeric_limits<uint32_t>::max()) {
-    if (m_seqMax != std::numeric_limits<uint32_t>::max()) {
-      if (m_seq >= m_seqMax) {
-        return; // we are totally done
-      }
-    }
-
-    seq = m_seq++;
-  }
-
-  //
-  shared_ptr<Name> nameWithSequence = make_shared<Name>(m_interestName);
-  nameWithSequence->appendSequenceNumber(seq);
-  //
-
-  // shared_ptr<Interest> interest = make_shared<Interest> ();
-  shared_ptr<Interest> interest = make_shared<Interest>();
-  interest->setNonce(m_rand->GetValue(0, std::numeric_limits<uint32_t>::max()));
-  interest->setName(*nameWithSequence);
-  time::milliseconds interestLifeTime(m_interestLifeTime.GetMilliSeconds());
-  interest->setInterestLifetime(interestLifeTime);
-
-  // NS_LOG_INFO ("Requesting Interest: \n" << *interest);
-  NS_LOG_INFO("> Interest for " << seq);
-
-  WillSendOutInterest(seq);
-
-  m_transmittedInterests(interest, this, m_face);
-  m_face->onReceiveInterest(*interest);
-
-  ScheduleNextPacket();
-}
-
-///////////////////////////////////////////////////
-//          Process incoming packets             //
-///////////////////////////////////////////////////
-
+/**
+ * Receives the data.
+ * Removes the interest packet on the queue.
+ * Checks if it is the last chunk of the object (for measurement purpose).
+ * Schedules next packet to request.
+ */
 void
 MobileUser::OnData(shared_ptr<const Data> data)
 {
-  if (!m_active)
-    return;
+  // // Do the regular processing
+  // Consumer::OnData(data);
+  // objectName = data->getName()
 
-  App::OnData(data); // tracing inside
+  // // Reduce the number of pending interest requests
+  // m_windowsSize--;
 
-  NS_LOG_FUNCTION(this << data);
+  // // Remove the interest from the packet queue
+  // m_interestQueue.remove(objectName);
 
-  // NS_LOG_INFO ("Received content object: " << boost::cref(*data));
+  // // Check if it is the last chunk
+  // ConcludeObjectDownload(objectName)
 
-  // This could be a problem......
-  uint32_t seq = data->getName().at(-1).toSequenceNumber();
-  NS_LOG_INFO("< DATA for " << seq);
-
-  int hopCount = 0;
-  auto ns3PacketTag = data->getTag<Ns3PacketTag>();
-  if (ns3PacketTag != nullptr) { // e.g., packet came from local node's cache
-    FwHopCountTag hopCountTag;
-    if (ns3PacketTag->getPacket()->PeekPacketTag(hopCountTag)) {
-      hopCount = hopCountTag.Get();
-      NS_LOG_DEBUG("Hop count: " << hopCount);
-    }
-  }
-
-  SeqTimeoutsContainer::iterator entry = m_seqLastDelay.find(seq);
-  if (entry != m_seqLastDelay.end()) {
-    m_lastRetransmittedInterestDataDelay(this, seq, Simulator::Now() - entry->time, hopCount);
-  }
-
-  entry = m_seqFullDelay.find(seq);
-  if (entry != m_seqFullDelay.end()) {
-    m_firstInterestDataDelay(this, seq, Simulator::Now() - entry->time, m_seqRetxCounts[seq], hopCount);
-  }
-
-  m_seqRetxCounts.erase(seq);
-  m_seqFullDelay.erase(seq);
-  m_seqLastDelay.erase(seq);
-
-  m_seqTimeouts.erase(seq);
-  m_retxSeqs.erase(seq);
-
-  m_rtt->AckSeq(SequenceNumber32(seq));
+  // // Schedules next packets
+  // ScheduleNextPacket();
 }
 
+/**
+ * 
+ */
 void
-MobileUser::OnTimeout(uint32_t sequenceNumber)
+MobileUser::OnTimeout(Name objectName)
 {
-  NS_LOG_FUNCTION(sequenceNumber);
-  // std::cout << Simulator::Now () << ", TO: " << sequenceNumber << ", current RTO: " <<
-  // m_rtt->RetransmitTimeout ().ToDouble (Time::S) << "s\n";
 
-  m_rtt->IncreaseMultiplier(); // Double the next RTO
-  m_rtt->SentSeq(SequenceNumber32(sequenceNumber),
-                 1); // make sure to disable RTT calculation for this sample
-  m_retxSeqs.insert(sequenceNumber);
-  ScheduleNextPacket();
 }
 
+/**
+ * 
+ */
+void
+MobileUser::SendPacket()
+{
+
+}
+
+/**
+ * 
+ */
 void
 MobileUser::WillSendOutInterest(uint32_t sequenceNumber)
 {
-  NS_LOG_DEBUG("Trying to add " << sequenceNumber << " with " << Simulator::Now() << ". already "
-                                << m_seqTimeouts.size() << " items");
 
-  m_seqTimeouts.insert(SeqTimeout(sequenceNumber, Simulator::Now()));
-  m_seqFullDelay.insert(SeqTimeout(sequenceNumber, Simulator::Now()));
-
-  m_seqLastDelay.erase(sequenceNumber);
-  m_seqLastDelay.insert(SeqTimeout(sequenceNumber, Simulator::Now()));
-
-  m_seqRetxCounts[sequenceNumber]++;
-
-  m_rtt->SentSeq(SequenceNumber32(sequenceNumber), 1);
 }
 
-// TODO
+/**
+ * 
+ */
 void
 MobileUser::ScheduleNextPacket()
 {
 }
+
+/**
+ * Adds a new object to the interest queue. It creates the name of the interest requests and puts it into the interest queue.
+ */
+void
+MobileUser::AddInterestObject(Name objectName, uint32_t chunks)
+{
+  // //create the interest name
+  // shared_ptr<Name> interestName = make_shared<Name>(objectName);
+
+  // //for each chunk, append the sequence number and add it to the interest queue
+  // for (int i = 0; i < chunks; i++) {
+  //   nameWithSequence->appendSequenceNumber(seq);
+  //   m_interestQueue.add(nameWithSequence);
+  // }
+}
+
+/**
+ * Get performance measurements of the download
+ */
+void
+MobileUser::ConcludeObjectDownload(Name objectName)
+{
+  // if (m_interestQueue.isLastChunk(objectName)) {
+  //   NS_LOG_INFO("Finished Dowlonading:" << objectName);
+  // }
+}
+
 
 } // namespace ndn
 } // namespace ns3
--- apps/ndn-mobile-user.hpp
+++ apps/ndn-mobile-user.hpp
@@ -21,93 +21,47 @@
 
 #ifndef NDN_MOBILE_USER_H
 #define NDN_MOBILE_USER_H
-
 #include "ns3/ndnSIM/model/ndn-common.hpp"
 
-#include "ndn-app.hpp"
-#include "ns3/ndnSIM/model/ndn-common.hpp"
-
-#include "ns3/random-variable-stream.h"
-#include "ns3/nstime.h"
-#include "ns3/data-rate.h"
-
-#include "ns3/ptr.h"
-
-#include "ns3/ndnSIM/model/ndn-common.hpp"
-#include "ns3/ndnSIM/utils/ndn-rtt-estimator.hpp"
-#include "ns3/ndnSIM/utils/ndn-fw-hop-count-tag.hpp"
-
-//TODO check
 #include "ndn-consumer.hpp"
-
-#include <set>
-#include <map>
-
-#include <boost/multi_index_container.hpp>
-#include <boost/multi_index/tag.hpp>
-#include <boost/multi_index/ordered_index.hpp>
-#include <boost/multi_index/member.hpp>
+#include "ns3/traced-value.h"
 
 namespace ns3 {
 namespace ndn {
 
 /**
  * @ingroup ndn-apps
- * @brief A mobile device that consumes, produces, and provides content.
- *
+ * @brief A mobile consumer that requests multiple objects with varied number of chunks.
  */
-class MobileUser : public App {
+class MobileUser : public Consumer {
 public:
   static TypeId
-  GetTypeId(void);
+  GetTypeId();
 
+  /* Default Constructor */
   MobileUser();
 
-  // inherited from NdnApp
-  virtual void
-  OnInterest(shared_ptr<const Interest> interest);
-
+  // From App
   virtual void
   OnData(shared_ptr<const Data> contentObject);
 
-  /**
-   * @brief Timeout event
-   * @param sequenceNumber time outed sequence number
-   */
   virtual void
-  OnTimeout(uint32_t sequenceNumber);
-
-  /**
-   * @brief Actually send packet
-   */
+  OnTimeout(Name objectName);
+  
   void
   SendPacket();
 
-  /**
-   * @brief An event that is fired just before an Interest packet is actually send out (send is
-   *inevitable)
-   *
-   * The reason for "before" even is that in certain cases (when it is possible to satisfy from the
-   *local cache),
-   * the send call will immediately return data, and if "after" even was used, this after would be
-   *called after
-   * all processing of incoming data, potentially producing unexpected results.
-   */
   virtual void
-  WillSendOutInterest(uint32_t sequenceNumber);
+  WillSendOutInterest(Name objectName);
 
-public:
-  typedef void (*LastRetransmittedInterestDataDelayCallback)(Ptr<App> app, uint32_t seqno, Time delay, int32_t hopCount);
-  typedef void (*FirstInterestDataDelayCallback)(Ptr<App> app, uint32_t seqno, Time delay, uint32_t retxCount, int32_t hopCount);
+  // New methods
+  virtual void
+  AddInterestObject();
+
+  virtual void
+  ConcludeObjectDownload();
 
 protected:
-  // inherited from Application base class.
-  virtual void
-  StartApplication(); // Called at time specified by Start
-
-  virtual void
-  StopApplication(); // Called at time specified by Stop
-
   /**
    * \brief Constructs the Interest packet and sends it using a callback to the underlying NDN
    * protocol
@@ -115,113 +69,15 @@ protected:
   virtual void
   ScheduleNextPacket();
 
-  /**
-   * \brief Checks if the packet need to be retransmitted becuase of retransmission timer expiration
-   */
-  void
-  CheckRetxTimeout();
-
-  /**
-   * \brief Modifies the frequency of checking the retransmission timeouts
-   * \param retxTimer Timeout defining how frequent retransmission timeouts should be checked
-   */
-  void
-  SetRetxTimer(Time retxTimer);
-
-  /**
-   * \brief Returns the frequency of checking the retransmission timeouts
-   * \return Timeout defining how frequent retransmission timeouts should be checked
-   */
-  Time
-  GetRetxTimer() const;
-
 private:
-  Name m_prefix;
-  Name m_postfix;
-  uint32_t m_virtualPayloadSize;
-  Time m_freshness;
+  Name[] m_interestQueue;
+  uint32_t m_windowsSize;
 
-  uint32_t m_signature;
-  Name m_keyLocator;
+  // These are the current used variables in ndn-consumer. But we have to change them to the interest queue above.
+  //uint32_t m_seq;      ///< @brief currently requested sequence number
+  //uint32_t m_seqMax;   ///< @brief maximum number of sequence number
+  //Name m_interestName;     ///< \brief NDN Name of the Interest (use Name)
 
-  Ptr<UniformRandomVariable> m_rand; ///< @brief nonce generator
-
-  uint32_t m_seq;      ///< @brief currently requested sequence number
-  uint32_t m_seqMax;   ///< @brief maximum number of sequence number
-  EventId m_sendEvent; ///< @brief EventId of pending "send packet" event
-  Time m_retxTimer;    ///< @brief Currently estimated retransmission timer
-  EventId m_retxEvent; ///< @brief Event to check whether or not retransmission should be performed
-
-  Ptr<RttEstimator> m_rtt; ///< @brief RTT estimator
-
-  Time m_offTime;          ///< \brief Time interval between packets
-  Name m_interestName;     ///< \brief NDN Name of the Interest (use Name)
-  Time m_interestLifeTime; ///< \brief LifeTime for interest packet
-
-  /// @cond include_hidden
-  /**
-   * \struct This struct contains sequence numbers of packets to be retransmitted
-   */
-  struct RetxSeqsContainer : public std::set<uint32_t> {
-  };
-
-  RetxSeqsContainer m_retxSeqs; ///< \brief ordered set of sequence numbers to be retransmitted
-
-  /**
-   * \struct This struct contains a pair of packet sequence number and its timeout
-   */
-  struct SeqTimeout {
-    SeqTimeout(uint32_t _seq, Time _time)
-      : seq(_seq)
-      , time(_time)
-    {
-    }
-
-    uint32_t seq;
-    Time time;
-  };
-  /// @endcond
-
-  /// @cond include_hidden
-  class i_seq {
-  };
-  class i_timestamp {
-  };
-  /// @endcond
-
-  /// @cond include_hidden
-  /**
-   * \struct This struct contains a multi-index for the set of SeqTimeout structs
-   */
-  struct SeqTimeoutsContainer
-    : public boost::multi_index::
-        multi_index_container<SeqTimeout,
-                              boost::multi_index::
-                                indexed_by<boost::multi_index::
-                                             ordered_unique<boost::multi_index::tag<i_seq>,
-                                                            boost::multi_index::
-                                                              member<SeqTimeout, uint32_t,
-                                                                     &SeqTimeout::seq>>,
-                                           boost::multi_index::
-                                             ordered_non_unique<boost::multi_index::
-                                                                  tag<i_timestamp>,
-                                                                boost::multi_index::
-                                                                  member<SeqTimeout, Time,
-                                                                         &SeqTimeout::time>>>> {
-  };
-
-  SeqTimeoutsContainer m_seqTimeouts; ///< \brief multi-index for the set of SeqTimeout structs
-
-  SeqTimeoutsContainer m_seqLastDelay;
-  SeqTimeoutsContainer m_seqFullDelay;
-  std::map<uint32_t, uint32_t> m_seqRetxCounts;
-
-  TracedCallback<Ptr<App> /* app */, uint32_t /* seqno */, Time /* delay */, int32_t /*hop count*/>
-    m_lastRetransmittedInterestDataDelay;
-  TracedCallback<Ptr<App> /* app */, uint32_t /* seqno */, Time /* delay */,
-                 uint32_t /*retx count*/, int32_t /*hop count*/> m_firstInterestDataDelay;
-
-  /// @endcond
 };
 
 } // namespace ndn
