09e77411117da5894ed29c2af7f3f5af903bd7c6
mblehmann <matheus@qualquer.inf.ufrgs.br>
2016-01-11 14:49:24 -0200

    Added the catalog
    The catalog records all created content. Also, starting compile tests.

---
+++ utils/ndn-catalog.hpp
@@ -0,0 +1,48 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2011-2015  Regents of the University of California.
+ *
+ * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#ifndef NDN_CATALOG_H
+#define NDN_CATALOG_H
+
+#include <list>
+
+#include "ns3/object.h"
+#include "ns3/random-variable.h"
+
+#include "ndn-cxx/name.hpp"
+
+using namespace std;
+
+class Catalog : public ns3::Object
+{
+public:
+  Catalog();
+  ~Catalog();
+  
+  void publishContent(Name object);
+  Name getLastObjectPublished();
+
+  uint32_t getCatalogSize();
+
+private:
+  list<Name> m_catalog;
+  
+};
+
+#endif // NDN_CATALOG_H
