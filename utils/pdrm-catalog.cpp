7944d957bae3ff6d3073567836bdf43cd11d30cf
Matheus Lehmann <mblehmann@inf.ufrgs.br>
2017-03-31 12:07:44 -0300

    Minor fix to the locality scenario

--- utils/pdrm-catalog.cpp
+++ utils/pdrm-catalog.cpp
@@ -95,7 +95,7 @@ PDRMCatalog::getLocalityRequestProbability(Name object, uint32_t domain)
   else if (objectIndex == m_domains - 1)
     locality = 1 - m_locality;
 
-  return (pow(objectIndex, -1*m_alpha) / m_totalProbability) * 20 * locality;
+  return (pow(objectIndex, -1*m_alpha) / m_totalProbability) * 10 * locality;
 }
 
 // default
